ea0d083213470bbc9aaeee45cc1d468a2eefb698
Patrik Natali <pat.natali@gmail.com>
2012-03-15 03:04:32 -0400

    initial commit

---
+++ send_email.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+import sys
+
+notified_user = "username"
+subject = "Motion Detected"
+body = "Motion was detected on your computer"
+
+if (len(sys.argv) > 1):
+	pic = sys.argv[1]
+
+gmail_user = "username"
+gmail_pwd = "password"
+
+def mail(to, subject, text, attach):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   part = MIMEBase('application', 'octet-stream')
+   part.set_payload(open(attach, 'rb').read())
+   Encoders.encode_base64(part)
+   part.add_header('Content-Disposition',
+           'attachment; filename="%s"' % os.path.basename(attach))
+   msg.attach(part)
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   # Should be mailServer.quit(), but that crashes...
+   mailServer.close()
+
+mail(notified_user, subject, body, pic)
